6cee966a2bc2c07b4fdf3c8554cb88b63487a18b
Him188 <Him188@mamoe.net>
2021-04-04 20:34:32 +0800

    GlobalControllerExceptionHandler infrastructure

---
+++ server/src/advice/GlobalControllerExceptionHandler.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2019-2021 Mamoe Technologies and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
+ *
+ * https://github.com/project-mirai/mirai-plugin-center/blob/master/LICENSE
+ */
+
+package net.mamoe.mirai.plugincenter.advice
+
+import net.mamoe.mirai.plugincenter.dto.ApiResp
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@ControllerAdvice
+class GlobalControllerExceptionHandler {
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception::class)
+    fun handle(e: Exception): ApiResp {
+        return ApiResp(500, e.message ?: e.toString(), null, e.stackTrace.map { it.toString() })
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException::class)
+    fun handle(e: IllegalArgumentException): ApiResp {
+        return ApiResp(400, e.message ?: e.toString(), null)
+    }
+}
