33eb7ebd22fe2c1e4d8fb66cc5c39818ab29c9d8
mzdluo123 <mzdluo123@foxmail.com>
2021-04-04 16:45:21 +0800

    add entity

---
+++ server/src/net/mamoe/mirai/plugincenter/model/RoleEntity.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2019-2021 Mamoe Technologies and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
+ *
+ * https://github.com/project-mirai/mirai-plugin-center/blob/master/LICENSE
+ */
+
+package net.mamoe.mirai.plugincenter.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "role", schema = "public", catalog = "plugins")
+public class RoleEntity {
+    private int id;
+    private String name;
+    private Integer parent;
+    private String desc;
+
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "parent")
+    public Integer getParent() {
+        return parent;
+    }
+
+    public void setParent(Integer parent) {
+        this.parent = parent;
+    }
+
+    @Basic
+    @Column(name = "desc")
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RoleEntity that = (RoleEntity) o;
+        return id == that.id && Objects.equals(name, that.name) && Objects.equals(parent, that.parent) && Objects.equals(desc, that.desc);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, parent, desc);
+    }
+}
--- server/src/services/PluginCenterUserService.kt
+++ server/src/services/PluginCenterUserService.kt
@@ -9,14 +9,19 @@
 
 package net.mamoe.mirai.plugincenter.services
 
+import org.springframework.security.core.userdetails.ReactiveUserDetailsPasswordService
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 
 @Service
-class PluginCenterUserService : ReactiveUserDetailsService {
+class PluginCenterUserService : ReactiveUserDetailsService,ReactiveUserDetailsPasswordService {
     override fun findByUsername(username: String?): Mono<UserDetails> {
+        TODO("Not yet implemented")
+    }
+
+    override fun updatePassword(user: UserDetails?, newPassword: String?): Mono<UserDetails> {
         TODO("Not yet implemented")
     }
 }
