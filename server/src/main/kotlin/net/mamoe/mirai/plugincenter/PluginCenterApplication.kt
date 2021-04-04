487b59e0ef540bab76b9d4bfebe203c05b7f75ae
Him188 <Him188@mamoe.net>
2021-04-04 15:00:36 +0800

    Add copyright

--- server/src/main/kotlin/net/mamoe/mirai/plugincenter/PluginCenterApplication.kt
+++ server/src/main/kotlin/net/mamoe/mirai/plugincenter/PluginCenterApplication.kt
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2019-2021 Mamoe Technologies and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
+ *
+ * https://github.com/project-mirai/mirai-plugin-center/blob/master/LICENSE
+ */
+
 package net.mamoe.mirai.plugincenter
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -7,5 +16,5 @@ import org.springframework.boot.runApplication
 class PluginCenterApplication
 
 fun main(args: Array<String>) {
-	runApplication<PluginCenterApplication>(*args)
+    runApplication<PluginCenterApplication>(*args)
 }
