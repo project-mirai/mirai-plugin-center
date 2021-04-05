d8defe37ff75893f76b308ece0050e98ebbc3f0f
Doctor_Yin <1921841421@qq.com>
2021-04-05 13:25:17 +0800

    邮箱判断

---
+++ server/src/services/DecideService.kt
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2019-2021 Mamoe Technologies and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
+ *
+ * https://github.com/project-mirai/mirai-plugin-center/blob/master/LICENSE
+ */
+
+package net.mamoe.mirai.plugincenter.services
+
+import org.springframework.stereotype.Service
+
+@Service
+class DecideService {
+    fun isEmail(str: String): Boolean {
+        return str.matches(Regex("^([a-zA-Z0-9_\\-\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})\$"))
+    }
+}
