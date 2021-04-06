2d5588c99f15213ec66b552969e16ec1cbb65e97
Karlatemp <karlatemp@vip.qq.com>
2021-04-06 18:22:13 +0800

    Authorization

--- server/src/advice/GlobalControllerExceptionHandler.kt
+++ server/src/advice/GlobalControllerExceptionHandler.kt
@@ -9,18 +9,21 @@
 
 package net.mamoe.mirai.plugincenter.advice
 
-import net.mamoe.mirai.plugincenter.PluginCenterApplication
 import net.mamoe.mirai.plugincenter.dto.ApiResp
+import net.mamoe.mirai.plugincenter.dto.Resp
+import net.mamoe.mirai.plugincenter.utils.authFailedReason
 import org.slf4j.Logger
 import org.springframework.beans.BeansException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.NestedRuntimeException
 import org.springframework.http.HttpStatus
+import org.springframework.security.access.AccessDeniedException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.support.WebExchangeBindException
+import org.springframework.web.server.ServerWebExchange
 
 
 @ControllerAdvice
@@ -29,11 +32,27 @@ class GlobalControllerExceptionHandler() {
     @Autowired
     private lateinit var logger: Logger
 
+    @ResponseBody
+    @ExceptionHandler(AccessDeniedException::class)
+    fun handle(exchange: ServerWebExchange, e: AccessDeniedException): ApiResp<Unit> {
+        if (logger.isTraceEnabled) {
+            logger.trace(e.toString(), e)
+        }
+        return ApiResp(
+            code = Resp.FORBIDDEN.code,
+            message = exchange.authFailedReason.msg,
+            null,
+            { e.stackTraceToString() }
+        )
+    }
 
     @ResponseBody
     @ExceptionHandler(Exception::class)
     fun handle(e: Exception): ApiResp<Unit> {
-        logger.error(e.toString(), e)
+        if (logger.isErrorEnabled) {
+            logger.error(e.toString(), e)
+        }
+
         when (e) {
             is NestedRuntimeException -> return ApiResp(400, e.mostSpecificCause.toString(), null, { e.stackTraceToString() })
             is BeansException -> return ApiResp(400, e.message ?: e.toString(), null, { e.stackTraceToString() })
@@ -46,7 +65,9 @@ class GlobalControllerExceptionHandler() {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(IllegalArgumentException::class)
     fun handle(e: IllegalArgumentException): ApiResp<Unit> {
-        logger.debug(e.toString(), e)
+        if (logger.isDebugEnabled) {
+            logger.debug(e.toString(), e)
+        }
         return ApiResp(400, e.message ?: e.toString(), null)
     }
 
@@ -54,7 +75,9 @@ class GlobalControllerExceptionHandler() {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(WebExchangeBindException::class)
     fun handleBadRequest(e: WebExchangeBindException): ApiResp<Unit> {
-        logger.debug(e.toString(), e)
+        if (logger.isDebugEnabled) {
+            logger.debug(e.toString(), e)
+        }
         return ApiResp(400, e.bindingResult.allErrors.joinToString { "${it.objectName}: ${it.defaultMessage}" } ?: e.toString(), null)
     }
 
---
+++ server/src/utils/auth.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2019-2021 Mamoe Technologies and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
+ *
+ * https://github.com/project-mirai/mirai-plugin-center/blob/master/LICENSE
+ */
+
+package net.mamoe.mirai.plugincenter.utils
+
+import net.mamoe.mirai.plugincenter.model.UserEntity
+import org.springframework.security.access.AccessDeniedException
+import org.springframework.web.server.ServerWebExchange
+
+val ServerWebExchange.loginUser: UserEntity?
+    get() = attributes["User"] as? UserEntity
+
+val ServerWebExchange.loginUserOrReject: UserEntity
+    get() = loginUser ?: throw AccessDeniedException(authFailedReason.msg)
+
+val ServerWebExchange.authFailedReason: AuthFailedReason
+    get() = attributes["AuthFailedReason"] as? AuthFailedReason ?: AuthFailedReason.UNKNOWN
+
+enum class AuthFailedReason(val msg: String) {
+    UNKNOWN("Unknown"),
+    GUEST("Unauthorized"),
+    BLOCKED("Blocked"),
+    TOKEN_EXPIRED("Token expired"),
+    TOKEN_NOT_FOUND("Invalid token"),
+    UNKNOWN_USER("Invalid session"),
+    ;
+}
