0b94b6f48a9645db8ef8213717868558e7797b96
Karlatemp <karlatemp@vip.qq.com>
2021-04-10 00:01:53 +0800

    SetOnly property

---
+++ server/src/utils/property.kt
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2019-2021 Mamoe Technologies and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
+ *
+ * https://github.com/project-mirai/mirai-plugin-center/blob/master/LICENSE
+ */
+
+package net.mamoe.mirai.plugincenter.utils
+
+import kotlin.reflect.KProperty
+
+class SetOnlyProperty<T>(
+    val operator: (T) -> Unit
+) {
+    operator fun getValue(thiz: Any?, p: KProperty<*>?): T = error("Set-only property")
+    operator fun setValue(thiz: Any?, p: KProperty<*>?, value: T) = operator(value)
+}
+
+fun <T> setonly(operator: (T) -> Unit) = SetOnlyProperty(operator)
+
