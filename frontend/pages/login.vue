66db5be11b81695d4a8eae640e15b51505785815
jasonczc <jasonczc@qq.com>
2021-04-10 23:23:49 +0800

    响应布局

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -2,18 +2,27 @@
 
   <v-row justify="center" align="center">
     <v-col cols="12" sm="9">
-      <v-card class="d-flex">
-        <v-col align-self="center" justify="center">
-           <p>dsadas</p>
-        </v-col>
-        <v-col>
-          <v-card-title>登录</v-card-title>
-          <v-card-text>
-            <login-form></login-form>
-          </v-card-text>
+      <v-card>
+        <v-row>
 
+          <v-col cols="6" md="6" sm="12">
+            <v-card class="transparent elevation-0">
+              <v-card-title >
+                Mirai Plugin Center
+              </v-card-title>
+              <v-card-subtitle>
+                请登录后进行操作
+              </v-card-subtitle>
+            </v-card>
+          </v-col>
 
-        </v-col>
+          <v-col cols="6" md="6" sm="12">
+            <v-card-title>登录</v-card-title>
+            <v-card-text>
+              <login-form></login-form>
+            </v-card-text>
+          </v-col>
+        </v-row>
 
       </v-card>
     </v-col>
