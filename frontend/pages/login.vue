3747046cfedab7ff6ed68965e5337ee08ccaccff
jasonczc <jasonczc@qq.com>
2021-04-10 22:58:06 +0800

    将loginform抽象为独立组件

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -4,34 +4,12 @@
     <v-col cols="12" sm="9">
       <v-card class="d-flex">
         <v-col align-self="center" justify="center">
-    <p>dsadas</p>
+           <p>dsadas</p>
         </v-col>
-        <v-col >
+        <v-col>
           <v-card-title>登录</v-card-title>
           <v-card-text>
-            <v-form
-              ref="form"
-              v-model="valid"
-              lazy-validation>
-              <v-text-field
-                v-model="email"
-                label="邮箱"
-                requiredp
-              ></v-text-field>
-
-              <v-text-field
-                type="password"
-                label="密码"
-                reqired/>
-
-              <v-btn
-                :disabled="!valid"
-                color="success"
-                class="mr-4">
-                登录
-              </v-btn>
-            </v-form>
-
+            <login-form></login-form>
           </v-card-text>
 
 
@@ -44,8 +22,10 @@
 </template>
 
 <script>
+import LoginForm from "../components/Auth/LoginForm";
 export default {
   name: "login",
+  components: {LoginForm},
   layout: "welcome"
 }
 </script>
