eda376cfde574bb970752548094b7d4a4228ab61
jasonczc <jasonczc@qq.com>
2021-04-11 10:55:09 +0800

    美化 Footer

--- frontend/components/Footer.vue
+++ frontend/components/Footer.vue
@@ -1,10 +1,34 @@
 <template>
-
   <v-footer
-
     app
+    color="transparent"
+    height="50"
+    inset
   >
-    <span>Mamoe &copy; {{ new Date().getFullYear() }}</span>
+    <v-card class="transparent elevation-0" width="100%">
+      <v-card-text>
+        <span class="float-left">Mamoe Technologies &copy; {{ new Date().getFullYear() }} </span>
+        <span class="float-right">
+          <v-btn
+            class="transparent elevation-0"
+            href="https://github.com/mamoe"
+            type="text"
+            :ripple="false"
+            plain
+            small>Github</v-btn>
+
+          <v-btn
+            class="transparent elevation-0"
+            href="https://github.com/mamoe"
+            type="text"
+            :ripple="false"
+            plain
+            small>源代码</v-btn>
+
+        </span>
+
+      </v-card-text>
+    </v-card>
   </v-footer>
 
 </template>
