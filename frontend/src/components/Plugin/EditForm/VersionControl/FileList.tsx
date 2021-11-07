0ff4c4de218740502e51ca2cf47f746298b73123
jasonczc <jasonczc@qq.com>
2021-11-07 10:10:40 +0800

    前端-编辑状态下文件下载支持 link #36

--- frontend/src/components/Plugin/EditForm/VersionControl/FileList.tsx
+++ frontend/src/components/Plugin/EditForm/VersionControl/FileList.tsx
@@ -1,20 +1,54 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
+import {Space, Table} from "antd";
 
 export interface VersionFilesProps{
     id:string
     version:string
     refresh?:()=>void
 }
+
+interface tableView {
+    filename:string
+}
+
 export default function(props:VersionFilesProps){
-    const [fileList, setFileList] = useState(Array<string>())
+    const [fileList, setFileList] = useState(Array<tableView>())
     useEffect( ()=>{
-        axios.get("/v1/plugins/"+props.id+"/"+props.version+"/").then(res=>setFileList(res.data.response))
+        axios.get("/v1/plugins/"+props.id+"/"+props.version+"/").then(res=>{
+            const arr = []
+            for(let i = 0; i < res.data.response.length; i++) {
+                arr.push({
+                    filename:res.data.response[i]
+                })
+            }
+            setFileList(arr)
+        })
     },[])
+    //TODO 在ENV中增加变量用于控制API
+    const API_URL = "http://localhost:8080"
+    const downloadFile = (filename:string)=>window.open(API_URL+'/v1/plugins/'+props.id+"/"+props.version+"/"+filename, '_blank')
+    const columns = [
+        {
+            title: '文件名',
+            dataIndex: 'filename',
+            key: 'filename',
+        },
+        {
+            title: '操作',
+            key: 'action',
+            render: (text:any, record:any) => (
+                <Space size="middle">
+                    <a onClick={()=>downloadFile(record.filename)}>下载 {record.filename}</a>
+                    <a>删除</a>
+                </Space>
+            ),
+        },
+    ];
     return (
         <>
             <h5>共{fileList.length}个附件</h5>
-            {fileList.map((item,index)=><h5 key={index}>{item}</h5>)}
+            <Table dataSource={fileList} columns={columns} />
         </>
     )
 }
